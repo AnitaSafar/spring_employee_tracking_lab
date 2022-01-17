ebfa44213dddacdc7d0a91281a7a1cdae9da2212
Anita Safar <ancsa0416@gmail.com>
2022-01-17 14:21:27 +0000

    repositories package added, EmployeeRepository created, setup done

--- employeetracking/src/test/java/com/codeclan/example/employeetracking/EmployeetrackingApplicationTests.java
+++ employeetracking/src/test/java/com/codeclan/example/employeetracking/EmployeetrackingApplicationTests.java
@@ -1,6 +1,7 @@
 package com.codeclan.example.employeetracking;
 
 import com.codeclan.example.employeetracking.models.Employee;
+import com.codeclan.example.employeetracking.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
