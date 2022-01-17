0f20fa1e231a38fb1fcd7c102864fe0cd0494c86
Anita Safar <ancsa0416@gmail.com>
2022-01-17 14:16:21 +0000

    createEmployee test added

--- employeetracking/src/test/java/com/codeclan/example/employeetracking/EmployeetrackingApplicationTests.java
+++ employeetracking/src/test/java/com/codeclan/example/employeetracking/EmployeetrackingApplicationTests.java
@@ -1,13 +1,24 @@
 package com.codeclan.example.employeetracking;
 
+import com.codeclan.example.employeetracking.models.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeetrackingApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee() {
+		Employee peter = new Employee("Peter Parker", 24, 43, "peter.p@yahoo.com");
+		employeeRepository.save(peter);
 	}
 
 }
