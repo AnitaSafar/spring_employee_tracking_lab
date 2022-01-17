23be433f878e6f7ca6c7ab6f5bc4f4989d880307
Anita Safar <ancsa0416@gmail.com>
2022-01-17 14:11:07 +0000

    POJO rules matched

--- employeetracking/src/main/java/com/codeclan/example/employeetracking/models/Employee.java
+++ employeetracking/src/main/java/com/codeclan/example/employeetracking/models/Employee.java
@@ -1,16 +1,29 @@
 package com.codeclan.example.employeetracking.models;
 
 public class Employee {
+    private long id;
     private String name;
     private int age;
     private int employeeNumber;
     private String email;
+
+    public Employee() {
+
+    }
 
     public Employee(String name, int age, int employeeNumber, String email) {
         this.name = name;
         this.age = age;
         this.employeeNumber = employeeNumber;
         this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
